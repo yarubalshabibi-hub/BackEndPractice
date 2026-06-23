2fd23425588e2a5975c1f8a1b01c62287cd9a0ca
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-23 16:30:54 +0400

    Added a project named pilot

--- FlightManagementSystem/Models/Pilot.cs
+++ FlightManagementSystem/Models/Pilot.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FlightManagementSystem.Models
+﻿namespace FMS.Models
 {
-    internal class Pilot
+    class Pilot
     {
+        public int PilotId { get; set; }
+        public string PilotName { get; set; }
+        public string PilotPhone { get; set; }
+        public string LicenseNumber { get; set; }
+        public int FlightHours { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Pilot(int pilotId, string pilotName, string pilotPhone, string licenseNumber)
+        {
+            PilotId = pilotId;
+            PilotName = pilotName;
+            PilotPhone = pilotPhone;
+            LicenseNumber = licenseNumber;
+            FlightHours = 0;     
+            IsAvailable = true; 
+        }
+
+        public void Display()
+        {
+            string status = IsAvailable ? "Available" : "Assigned";
+            Console.WriteLine($"  ID: {PilotId} | Name: {PilotName} | License: {LicenseNumber} | Hours: {FlightHours} | Status: {status}");
+        }
     }
 }
