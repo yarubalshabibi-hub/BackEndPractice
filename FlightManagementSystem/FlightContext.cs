70bf97d36cc9ab84f7fe780987b5824c9be6d419
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-24 16:32:24 +0400

    Added a new file named flightContext

---
+++ FlightManagementSystem/FlightContext.cs
@@ -0,0 +1,16 @@
+﻿using FlightManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem
+{
+    public class FlightContext
+    {
+        public List<Passenger> Passengers { get; set; } 
+        public List<Pilot> Pilots { get; set; } 
+        public List<Aircraft> Aircrafts { get; set; } 
+        public List<Flight> Flights { get; set; } 
+        public List<Booking> Bookings { get; set; } 
+    }
+}
