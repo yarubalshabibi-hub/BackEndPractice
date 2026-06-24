140dc7f488755d6e0f8e09e5ef52ddbca63ac7dc
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-24 11:44:23 +0400

    Declared the property names for the pilot

--- FlightManagementSystem/Models/Pilot.cs
+++ FlightManagementSystem/Models/Pilot.cs
@@ -1,6 +1,10 @@
-﻿namespace FMS.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
 {
-    class Pilot
+    public class Pilot
     {
         public int PilotId { get; set; }
         public string PilotName { get; set; }
