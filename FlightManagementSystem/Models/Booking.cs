915177ef111cb3fe4e6dd048717b91409afa2461
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-24 11:43:11 +0400

    Declared the property names for the booking

--- FlightManagementSystem/Models/Booking.cs
+++ FlightManagementSystem/Models/Booking.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace FlightManagementSystem.Models
 {
-    internal class Booking
-    {
+    public class Booking
+        {
+        public int BookingId { get; set; }
+        public int PassengerId { get; set; }
+        public int FlightId { get; set; }
+        public string SeatNumber { get; set; }
+        public string BookingDate { get; set; }  
+        public decimal TotalPrice { get; set; }  
+        public string Status { get; set; }   
+
+        public Booking(int bookingId, int passengerId, int flightId,
+                       string seatNumber, decimal totalPrice)
+        {
+            BookingId = bookingId;
+            PassengerId = passengerId;
+            FlightId = flightId;
+            SeatNumber = seatNumber;
+            BookingDate = DateTime.Now.ToString("yyyy-MM-dd");  
+            TotalPrice = totalPrice;
+            Status = "Confirmed";  
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"  BookingID: {BookingId} | PassengerID: {PassengerId} | FlightID: {FlightId} | Seat: {SeatNumber} | Date: {BookingDate} | Price: OMR {TotalPrice:F2} | Status: {Status}");
+        }
     }
 }
