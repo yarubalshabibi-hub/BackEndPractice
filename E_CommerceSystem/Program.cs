4ea3ee5ba16ad68fff7a616fff19822c737ead84
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-07-02 09:01:55 +0400

    I Added values in Program Class

---
+++ E_CommerceSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
